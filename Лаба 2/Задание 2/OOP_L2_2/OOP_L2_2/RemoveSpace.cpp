14a36a2f204a3bc7aa1cb8b59ff24f3e9d62174b
CNewStudent <takhir.learn@gmail.com>
2015-11-01 19:35:36 +0300

    Обновление

--- Лаба 2/Задание 2/OOP_L2_2/OOP_L2_2/RemoveSpace.cpp
+++ Лаба 2/Задание 2/OOP_L2_2/OOP_L2_2/RemoveSpace.cpp
@@ -7,38 +7,30 @@ using namespace std;
 string RemoveExtraSpaces(string const &arg)
 {
 	string result;
-	result.reserve(arg.size());
-
-	if (arg.size() == 0)
+	bool isSpaces = false;
+	
+	for (size_t i = 0; i < arg.size(); i++)
 	{
-		result = "";
+		if (arg[i] != ' ')
+		{
+			result += arg[i];
+			isSpaces = true;
+		}
+
+		if (isSpaces && arg[i] == ' ')
+		{
+			result += arg[i];
+			isSpaces = false;
+		}
+	}
+	
+	if (result.empty())
+	{
 		return result;
 	}
-	const char *space = " ";
-	
-	for (unsigned int i = 0; i < arg.size(); i++)
-	{
-		if (arg[i] == *space)
-		{
-			while (arg[i + 1] == *space)
-			{
-				i++;
-			}
-			result.push_back(arg[i]);
-		}
-		else
-		{
-			result.push_back(arg[i]);
-		}
-	}
-
-	if (result[result.size() - 1] == *space)
+	if (result.back() == ' ')
 	{
 		result.pop_back();
-	}
-	if (result[0] == *space)
-	{
-		result.erase(result.begin());
 	}
 	return result;
 }
--- Лаба 2/Задание 2/OOP_L2_2/TheApplicationTests/Tests.cpp
+++ Лаба 2/Задание 2/OOP_L2_2/TheApplicationTests/Tests.cpp
@@ -9,7 +9,7 @@ BOOST_AUTO_TEST_CASE(EmptyString)
 	string str_One, str_Two;
 	str_One = "";
 	str_Two = RemoveExtraSpaces(str_One);
-	BOOST_CHECK(str_Two.size() == 0);
+	BOOST_CHECK(str_Two.empty());
 }
 
 BOOST_AUTO_TEST_CASE(OneSpace)
@@ -17,7 +17,7 @@ BOOST_AUTO_TEST_CASE(OneSpace)
 	string str_One, str_Two;
 	str_One = " ";
 	str_Two = RemoveExtraSpaces(str_One);
-	BOOST_CHECK(str_Two.size() == 0);
+	BOOST_CHECK(str_Two.empty());
 }
 
 BOOST_AUTO_TEST_CASE(SpaceBegin)
@@ -25,7 +25,7 @@ BOOST_AUTO_TEST_CASE(SpaceBegin)
 	string str_One, str_Two;
 	str_One = "       test";
 	str_Two = RemoveExtraSpaces(str_One);
-	BOOST_CHECK(str_Two.size() == 4);
+	BOOST_CHECK(str_Two == "test");
 }
 
 BOOST_AUTO_TEST_CASE(SpaceEnd)
@@ -33,7 +33,7 @@ BOOST_AUTO_TEST_CASE(SpaceEnd)
 	string str_One, str_Two;
 	str_One = " test    ";
 	str_Two = RemoveExtraSpaces(str_One);
-	BOOST_CHECK(str_Two.size() == 4);
+	BOOST_CHECK(str_Two == "test");
 }
 
 BOOST_AUTO_TEST_CASE(MuchSpace)
@@ -41,5 +41,13 @@ BOOST_AUTO_TEST_CASE(MuchSpace)
 	string str_One, str_Two;
 	str_One = " t  t  t  ";
 	str_Two = RemoveExtraSpaces(str_One);
-	BOOST_CHECK(str_Two.size() == 5);
+	BOOST_CHECK(str_Two == "t t t");
+}
+
+BOOST_AUTO_TEST_CASE(MuchSpace2)
+{
+	string str_One, str_Two;
+	str_One = "Hello          world";
+	str_Two = RemoveExtraSpaces(str_One);
+	BOOST_CHECK(str_Two == "Hello world");
 }
